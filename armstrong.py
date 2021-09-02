4001c1a66c5a5c7d59e6f03dca5a98106d20a14c
hchouhan <43321682+hrushabh8899@users.noreply.github.com>
2021-09-01 23:00:20 -0400

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,16 @@
+print("Enter the range :")
+n = int(input())
+
+for num in range(100, n+1):
+
+   order = len(str(num))
+   sum = 0
+
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)
