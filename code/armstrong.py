03a084cac1d44cd88eb4ecb0ec44032ce196e79b
hiralbhanu <hiralbhanushali13@gmail.com>
2021-09-02 00:06:10 -0400

    Update armstrong.py

--- code/armstrong.py
+++ code/armstrong.py
@@ -9,7 +9,7 @@ for num in range(100, n+1):
    temp = num
    while temp > 0:
        digit = temp % 10
-       sum += digit ** order
+       sum += digit ** 3
        temp //= 10
 
    if num == sum:
