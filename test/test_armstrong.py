67d70bcf8798829548b16421ee4cc047eb3f10c2
Angela <angelaho0504@gmail.com>
2021-09-02 23:52:16 -0400

    update test

--- test/test_armstrong.py
+++ test/test_armstrong.py
@@ -4,15 +4,11 @@ import sys
 __dirname = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, __dirname + '/..')
 
-from code import armstrong
-
 def test_valid():
 
-    assert armstrong.armstrong(999) == [1, 153, 370,371, 407]
+    armstrong.input = lambda: 999
+    output = armstrong.armstrong()
+    assert output == [1, 153, 370,371, 407]       
 
-#     armstrong.input = lambda: 999
-#     output = armstrong.armstrong()
-#     assert output == [1, 153, 370,371, 407]       
-
-# def teardown_method(self, method):
-#     armstrong.input = input  
+def teardown_method(self, method):
+    armstrong.input = input  
