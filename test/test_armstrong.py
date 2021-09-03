182d01eeb5d65977b50c7f9e18f1bc9fdb596b8c
Angela <angelaho0504@gmail.com>
2021-09-02 22:23:42 -0400

    update test

--- test/test_armstrong.py
+++ test/test_armstrong.py
@@ -1,10 +1,10 @@
-import armstong
+import armstrong
 
 def test_valid(self):
 
-    armstong.input = lambda: 999
+    armstrong.input = lambda: 999
     output = armstong.armstrong()
     assert output == [1, 153, 370,371, 407]       
 
 def teardown_method(self, method):
-    armstong.input = input  
+    armstrong.input = input  
