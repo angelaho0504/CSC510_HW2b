ddd293a274455f18c75c905b82d65e0431dcab6a
Angela <angelaho0504@gmail.com>
2021-09-02 22:13:01 -0400

    update test

--- test/test.py
+++ test/test.py
@@ -1,10 +1,10 @@
-import module
+# import module
 
-def test_function(self):
+def test_armstrong(self):
 
-    module.input = lambda: 999
-    output = module.armstrong()
+    # module.input = lambda: 999
+    # output = module.armstrong()
     assert output == [1, 153, 370,371, 407]       
 
-def teardown_method(self, method):
-    module.input = input  
+# def teardown_method(self, method):
+#     module.input = input  
