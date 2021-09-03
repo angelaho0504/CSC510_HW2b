7815301513cce00c5883852fc618dafccf862610
Angela <angelaho0504@gmail.com>
2021-09-02 23:38:53 -0400

    update test

--- test/test_armstrong.py
+++ test/test_armstrong.py
@@ -3,7 +3,7 @@ from code.armstrong import armstrong
 
 def test_valid():
 
-    assert armstrong.armstrong(999) = [1, 153, 370,371, 407]
+    assert armstrong.armstrong(999) == [1, 153, 370,371, 407]
 
 #     armstrong.input = lambda: 999
 #     output = armstrong.armstrong()
