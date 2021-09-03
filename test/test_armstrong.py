73c8c7f26e011a974b529fb88f60b5b006ea191f
Angela <angelaho0504@gmail.com>
2021-09-02 23:19:16 -0400

    update test

--- test/test_armstrong.py
+++ test/test_armstrong.py
@@ -3,4 +3,4 @@ import code
 from code.armstrong import armstrong
 
 def test_valid():
-    assert armstrong.armstrong(999) = [1, 153, 370,371, 407]
+    assert armstrong.armstrong(999) == [1, 153, 370,371, 407]
