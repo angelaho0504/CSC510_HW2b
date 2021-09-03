014ef6e069d4c048d026e13a799c5a6fb214bf81
Angela <angelaho0504@gmail.com>
2021-09-02 23:18:12 -0400

    update test

--- code/armstrong.py
+++ code/armstrong.py
@@ -1,7 +1,7 @@
-def armstrong():
+def armstrong(n):
   armstrong_list = []
-  print("Enter the range :")
-  n = int(input())
+  # print("Enter the range :")
+  # n = int(input())
 
   for num in range(1, n+1):
 
--- test/test_armstrong.py
+++ test/test_armstrong.py
@@ -1,6 +1,6 @@
-import mock
+
 import code
+from code.armstrong import armstrong
 
 def test_valid():
-    with mock.patch.object(__builtins__, 'input', lambda: 999):
-        assert armstrong.function() == [1, 153, 370,371, 407]  
+    assert armstrong.armstrong(999) = [1, 153, 370,371, 407]
